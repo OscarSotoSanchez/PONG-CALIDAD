c17c4b1ce192191c67d70f6a233653d77c5d6791
Hgmoa <hugo.97.vk@gmail.com>
2018-02-12 12:15:42 +0100

    AutoCommit made by gradle task on 2101-06-21

--- desktop/src/com/mygdx/pong/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/pong/desktop/DesktopLauncher.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.pong.Game.GamePong;
 
+import java.time.Instant;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
